7c047d1ac567807b50245fc664e7f415b4dc4267
Rick <rickpowell1311@gmail.com>
2019-01-13 18:30:53 +0000

    Temporary fix for long-running app whilst running on docker

---
+++ src/Websocket.Relay.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Websocket.Relay.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var relay = Relay.Load(new ConsoleLogger(), RelayConfiguration.Default);
+            await relay.Run(7890, RunMode.Test);
+
+            System.Threading.Thread.Sleep(TimeSpan.FromHours(24));
+
+            //System.Console.WriteLine("Press any key to quit");
+            //System.Console.Read();
+        }
+    }
+
+    public class ConsoleLogger : ILogger
+    {
+        public async Task Info(string message)
+        {
+            await Task.CompletedTask;
+            System.Console.WriteLine(message);
+        }
+    }
+}
